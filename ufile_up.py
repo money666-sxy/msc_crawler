33dae305591cb2c06692fac67db80cb967ba2b27
money666-sxy <53988160+money666-sxy@users.noreply.github.com>
2020-09-20 21:08:51 +0800

    Update ufile_up.py

--- ufile_up.py
+++ ufile_up.py
@@ -3,6 +3,14 @@ from ufile import filemanager
 from ufile import bucketmanager
 from ufile import config
 
+public_bucket = ''  # 公共空间名称(不加后缀)
+# private_bucket = '' # 私有空间名称(不加后缀)
+localfile = ''  # 本地文件名
+put_key = ''  # 上传文件在空间中的名称
+region = ''
+public_key = ''
+private_key = ''
+
 
 # 设置上传host后缀,外网可用后缀形如 .cn-bj.ufileos.com（cn-bj为北京地区，其他地区具体后缀可见控制台：对象存储-单地域空间管理-存储空间域名）
 config.set_default(uploadsuffix='.{0}.{1}.ufileos.com'.format(public_bucket,region))
@@ -15,13 +23,6 @@ config.set_default(expires=60)
 # 设置上传文件是否校验md5
 config.set_default(md5=True)
 
-public_bucket = ''  # 公共空间名称(不加后缀)
-# private_bucket = '' # 私有空间名称(不加后缀)
-localfile = ''  # 本地文件名
-put_key = ''  # 上传文件在空间中的名称
-region = ''
-public_key = ''
-private_key = ''
 
 
 def put_file(localfile: str, put_key: str):
